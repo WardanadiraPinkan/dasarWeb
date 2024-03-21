8b4438bd1c0f855ef50bfd6603786e809c8ff366
WardanadiraPinkan <wardanadira04@gmail.com>
2024-03-21 11:01:46 +0700

    Pertemuan 5 - Praktikum kumpulan variabel dan time

---
+++ Pertemuan 5/praktik_php/string2.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Baris\nbaru <br>"; //soal 10.a
+echo 'Baris\nbaru <br>'; //soal 10.b
+echo "Halo\rDunia <br>"; //soal 10.c
+echo 'Halo\rDunia <br>'; //soal 10.c
+
+echo "<pre>Halo\tDunia!</pre>"; //soal 10.e
+echo '<pre>Halo\tDunia!</pre>'; //soal 10.e
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 10.g
+echo 'Katakanlah \"Tidak pada narkoba!\' <br>'; //soal 10.g
+
+?>
